bb6644c5a37fcd5a25b17b7ee5411f5d7665dfd1
Mateusz <46190977+Matthieusz@users.noreply.github.com>
2024-03-26 18:15:17 +0100

    added task7 in zad4

--- JPO/zad4/main.cpp
+++ JPO/zad4/main.cpp
@@ -1,9 +1,80 @@
+// Forward declaration of the operator<< function
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <limits>
+#include <vector>
 #include "vector.h"
 using namespace std;
+
+class Matrix {
+private:
+    std::vector<std::vector<int>> data;
+public:
+    Matrix(int m, int n) : data(m, std::vector<int>(n)) {}
+
+    std::vector<int>& operator[](int index) {
+        return data[index];
+    }
+
+    bool operator==(const Matrix& other) const {
+        if (data.size() != other.data.size() || data[0].size() != other.data[0].size()) {
+            return 0;
+        }
+
+        for (size_t i = 0; i < data.size(); i++) {
+            for (size_t j = 0; j < data[0].size(); j++) {
+                if (data[i][j] != other.data[i][j]) {
+                    return 0;
+                }
+            }
+        }
+
+        return 1;
+    }
+
+    Matrix operator*(const Matrix& other) const {
+        int m = data.size();
+        int n = data[0].size();
+        int p = other.data[0].size();
+
+        Matrix result(m, p);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < p; j++) {
+                for (int k = 0; k < n; k++) {
+                    result[i][j] += data[i][k] * other.data[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    Matrix& operator*=(int k) {
+        for (auto& row : data) {
+            for (auto& element : row) {
+                element *= k;
+            }
+        }
+        return *this;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix);
+};
+
+std::ostream& operator<<(ostream& os, const Matrix& matrix) {
+    int m = matrix.data.size();
+    int n = matrix.data[0].size();
+
+    os << "Matrix (" << m << "x" << n << "):" << endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            os << matrix.data[i][j] << " ";
+        }
+        os << endl;
+    }
+    return os;
+}
 
 void zad1() {
     typedef int T;
@@ -52,6 +123,34 @@ void zad1() {
     cin.ignore(2);
 }
 
+void zad7() {
+    int m, n, p;
+    cout << "Podaj wymiary macierzy A (m x n): ";
+    cin >> m >> n;
+    Matrix A(m, n);
+    cout << "Podaj elementy macierzy A:" << endl;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+    cout << "Podaj wymiary macierzy B (n x p): ";
+    cin >> n >> p;
+    Matrix B(n, p);
+    cout << "Podaj elementy macierzy B:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < p; j++) {
+            cin >> B[i][j];
+        }
+    }
+    cout << "A:" << endl << A << endl;
+    cout << "B:" << endl << B << endl;
+    cout << "A * B:" << endl << A * B << endl;
+    cout << "A * 2:" << endl << (A *= 2) << endl;
+    cout << "A == B: " << (A == B) << endl;
+    cin.ignore(2);
+}
+
 int main() {
     do {
         int task;
@@ -65,7 +164,7 @@ int main() {
             break;
         case 7:
             cout << "Zadanie 7" << endl;
-            //zad7();
+            zad7();
             break;
         default:
             cout << "Nie ma takiego zadania" << endl;
